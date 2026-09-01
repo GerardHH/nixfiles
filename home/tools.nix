8fd1dd7f0f030cb31019a438ea639623af387904
Gerard Heshusius <gh.heshusius@gmail.com>
2026-09-01 19:30:13 +0200

    feat: Add btop

--- home/tools.nix
+++ home/tools.nix
@@ -5,10 +5,12 @@
 }:
 {
   home.packages = with pkgs; [
+    btop
     lazygit
   ];
 
   xdg.configFile = {
+    "btop".source = link "config/btop";
     "lazygit".source = link "config/lazygit";
   };
 }
