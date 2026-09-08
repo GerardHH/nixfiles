947416ab7eb28308e22af745424296fb5d6b6c42
Gerard Heshusius <gh.heshusius@gmail.com>
2026-09-08 14:59:22 +0200

    fix: Claude: Deny any writes in projects

--- home/claude.nix
+++ home/claude.nix
@@ -10,7 +10,7 @@
 
   # Link only actuall settings, so that claude-code can dump its own data in .claude folder
   home.file = {
-    ".claude/settings.json".source = link "/config/claude/settings.json";
+    ".claude/settings.json".source = link "config/claude/settings.json";
     ".claude/CLAUDE.md".source = link "config/claude/CLAUDE.md";
   };
 }
