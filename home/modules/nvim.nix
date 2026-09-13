c8e390b5d6a6e94ab8fab838545bad04bfeae781
Gerard Heshusius <gh.heshusius@gmail.com>
2026-09-13 08:35:17 +0200

    feat: ripgrep: Deploy config

--- home/modules/nvim.nix
+++ home/modules/nvim.nix
@@ -6,13 +6,13 @@
 {
   imports = [
     ./fzf.nix
+    ./ripgrep.nix
   ];
 
   home.packages = with pkgs; [
     neovim
 
     fd # fzf-lua file provider
-    ripgrep # fzf-lua grep provider
   ];
 
   xdg.configFile."nvim".source = link "config/nvim";
